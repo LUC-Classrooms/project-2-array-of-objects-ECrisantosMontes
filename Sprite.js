d9711a269794aa22414926cc1998de7e17a0997f
ECrisantosMontes <156939016+ECrisantosMontes@users.noreply.github.com>
2024-04-15 04:28:12 +0000

    updated 4/14/24

--- Sprite.js
+++ Sprite.js
@@ -1,18 +1,30 @@
-//add code fro class contructor
+//add code for class contructor
 function Sprite (tempX, tempY){
     this.x = tempX;
     this.y = tempY;
-    this.XSpeed = random(-2, 2);
-    this.YSpeed = random(-2, 2);
+    this.xSpeed = random(-2, 2);
+    this.ySpeed = random(-2, 2);
 
-    this.move = finction(){
-        this.x += this.XSpeed;
-        this.y += this.YSpeed;
+ this.move = function(){
+        this.x += this.xSpeed;
+        this.y += this.ySpeed;
         if(this.x > width || this.x < 0){
             this.xSpeed *= -1;
         }
+        if(this.y > height || this.y < 0){
+            this.ySpeed *= -1;
+        }
     }
+
+this.display = function(){
+push(); // creates new layer
+translate(this.x, this.y);// moves new layers origin point on canvas
+rectMode(CENTER);
+fill(200, 50, 200, 50);
+rect(0, 0, 75); // rectangle
+ellipse(-20, -10, 5, 10)
+ellipse(20, -10, 5, 10)
+pop();// dispose layer
 }
-this.display = function() {
-    
+
 }
